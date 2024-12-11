5e79938aef4fd4571059df403ff7ca5c1b40b45c
Caique-Gargel <caiquegargeloliveira@gmail.com>
2024-12-11 16:57:32 -0300

    iniciando criação da interface

---
+++ src/Components/layout/Bloco.js
@@ -0,0 +1,13 @@
+import styles from "./Bloco.module.css"
+
+function Bloco({texto,titulo})
+{
+    return(
+        <div className={styles.bloco}>
+            <h1>{titulo}</h1>
+            <p>{texto}</p>
+        </div>
+        
+    );
+}
+export default Bloco
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,8 +1,13 @@
 import styles from "./Home.module.css"
-
+import logo from "../../5.png"
+import Bloco from "../layout/Bloco";
 function Home(){
     return(
-        <p>Home</p>
+        <div>
+            <img className={styles.logo} src={logo} alt="cbloldle"></img>
+            <Bloco texto="dsadsadsadsads" titulo="titulo"/>
+        </div>
+        
     )
 }
 export default Home;
